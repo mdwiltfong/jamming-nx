8a5dc6fc38a939fb0d9eafe81fd050c81f8ec1e5
mdwiltfong <wiltfong.michael1@gmail.com>
2023-07-09 00:19:07 -0400

    small refactor

--- jamming-app/apps/jamming-api/src/main.ts
+++ jamming-app/apps/jamming-api/src/main.ts
@@ -2,8 +2,8 @@ import app from './server';
 
 const PORT = process.env.PORT || 3000;
 
-const httpServer = app.listen(PORT, () => {
+export const httpServer = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/ `);
 });
-export { httpServer };
+
 export default app;
