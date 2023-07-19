070f80460425a5c94a08e47903d7c54b26ef2ff8
mdwiltfong <wiltfong.michael1@gmail.com>
2023-07-19 08:34:01 -0400

    userRoutes

---
+++ jamming-app/apps/jamming-api/src/routes/userRouter.ts
@@ -0,0 +1,24 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { Model } from '../db/helpers/db.helper';
+import { User } from '../db/helpers/models/User';
+const userModel = new Model<User>('users');
+const userRouter = Router();
+
+userRouter.get(
+  '/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const user = await userModel.findDocument(
+        {
+          _id: new Object(id),
+        },
+        { name: 'users' }
+      );
+      return res.status(200).json(user);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+export default userRouter;
--- jamming-app/apps/jamming-api/src/server.ts
+++ jamming-app/apps/jamming-api/src/server.ts
@@ -1,33 +1,13 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
-import { Model } from './db/helpers/db.helper';
-import { User } from './db/helpers/models/User';
-import { ObjectId } from 'mongodb';
 import morgan from 'morgan';
-
-const userModel = new Model<User>('users');
+import userRouter from './routes/userRouter';
 const app: Express = express();
 app.use(morgan('tiny'));
 app.get('/status', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello World' });
 });
 
-app.get(
-  '/users/:id',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { id } = req.params;
-      const user = await userModel.findDocument(
-        {
-          _id: new ObjectId(id),
-        },
-        { name: 'users' }
-      );
-      return res.status(200).json(user);
-    } catch (error) {
-      return next(error);
-    }
-  }
-);
+app.use('/users', userRouter);
 
 app.use(
   (
