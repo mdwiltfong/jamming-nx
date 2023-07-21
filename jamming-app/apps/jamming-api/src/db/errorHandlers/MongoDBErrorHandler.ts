28d212ac2afa9e3f5e79e4500f55d243bdbe2e4a
mdwiltfong <wiltfong.michael1@gmail.com>
2023-07-20 22:10:22 -0400

    modified schema to take UUID as a string

--- jamming-app/apps/jamming-api/src/db/errorHandlers/MongoDBErrorHandler.ts
+++ jamming-app/apps/jamming-api/src/db/errorHandlers/MongoDBErrorHandler.ts
@@ -1,12 +1,3 @@
-import colors from 'colors';
-import {
-  MongoNetworkError,
-  MongoParseError,
-  MongoServerSelectionError,
-  MongoNetworkTimeoutError,
-  MongoServerError,
-} from 'mongodb';
-
 export default class MongoDBErrorHandler extends Error {
   constructor(message: string) {
     super(message);
--- jamming-app/apps/jamming-api/src/db/helpers/mockData.ts
+++ jamming-app/apps/jamming-api/src/db/helpers/mockData.ts
@@ -1,37 +1,37 @@
-import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
 
 // Mockusers with "password" as their password
 const mockUsers = [
   {
-    _id: new ObjectId(1),
+    _id: '40fdf6ee-8403-4f5a-bc62-020dab7c43a8',
     firstName: 'Alice',
     lastName: 'Smith',
     email: 'alice.smith@example.com',
     password: 'U2FsdGVkX19nR6kAxLmriGbdDMIS/RF3NyrQyD5NwA4=',
   },
   {
-    _id: new ObjectId(2),
+    _id: '987faee9-8a1b-4651-9f58-d82933e5053b',
     firstName: 'Bob',
     lastName: 'Johnson',
     email: 'bob.johnson@example.com',
     password: 'U2FsdGVkX19nR6kAxLmriGbdDMIS/RF3NyrQyD5NwA4=',
   },
   {
-    _id: new ObjectId(3),
+    _id: '269ae06f-fb32-4935-85e1-3df76e42d92f',
     firstName: 'Charlie',
     lastName: 'Brown',
     email: 'charlie.brown@example.com',
     password: 'U2FsdGVkX19nR6kAxLmriGbdDMIS/RF3NyrQyD5NwA4=',
   },
   {
-    _id: new ObjectId(4),
+    _id: '91659038-4dfe-4bc4-a997-31f6f1b2598d',
     firstName: 'David',
     lastName: 'Wilson',
     email: 'david.wilson@example.com',
     password: 'U2FsdGVkX19nR6kAxLmriGbdDMIS/RF3NyrQyD5NwA4=',
   },
   {
-    _id: new ObjectId(5),
+    _id: 'adf3677a-5377-49ca-8338-071daa0ed211',
     firstName: 'Emily',
     lastName: 'Davis',
     email: 'emily.davis@example.com',
@@ -40,8 +40,8 @@ const mockUsers = [
 ];
 const mockPlaylists = [
   {
-    _id: new ObjectId(1),
-    userId: new ObjectId(1),
+    _id: uuidv4(),
+    userId: mockUsers[0]._id,
     name: 'Playlist 1',
     spotifyUserId: '6rqhFgbbKwnb9MLmUQDhG6',
     spotifyPlayListId: '37i9dQZF1DXcBWIGoYBM5M',
@@ -49,8 +49,8 @@ const mockPlaylists = [
       'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
   },
   {
-    _id: new ObjectId(2),
-    userId: new ObjectId(2),
+    _id: uuidv4(),
+    userId: mockUsers[1]._id,
     name: 'Playlist 2',
     spotifyUserId: '6rqhFgbbKwnb9MLmUQDhG6',
     spotifyPlayListId: '37i9dQZF1DXcBWIGoYBM5M',
@@ -58,8 +58,8 @@ const mockPlaylists = [
       'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
   },
   {
-    _id: new ObjectId(3),
-    userId: new ObjectId(3),
+    _id: uuidv4(),
+    userId: mockUsers[2]._id,
     name: 'Playlist 3',
     spotifyUserId: '6rqhFgbbKwnb9MLmUQDhG6',
     spotifyPlayListId: '37i9dQZF1DXcBWIGoYBM5M',
@@ -67,8 +67,8 @@ const mockPlaylists = [
       'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
   },
   {
-    _id: new ObjectId(4),
-    userId: new ObjectId(4),
+    _id: uuidv4(),
+    userId: mockUsers[3]._id,
     name: 'Playlist 4',
     spotifyUserId: '6rqhFgbbKwnb9MLmUQDhG6',
     spotifyPlayListId: '37i9dQZF1DXcBWIGoYBM5M',
@@ -76,8 +76,8 @@ const mockPlaylists = [
       'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
   },
   {
-    _id: new ObjectId(5),
-    userId: new ObjectId(5),
+    _id: uuidv4(),
+    userId: mockUsers[4]._id,
     name: 'Playlist 5',
     spotifyUserId: '6rqhFgbbKwnb9MLmUQDhG6',
     spotifyPlayListId: '37i9dQZF1DXcBWIGoYBM5M',
