65d096b0f637d13f39f8aef9172a8566bd66aab3
mdwiltfong <wiltfong.michael1@gmail.com>
2023-08-01 19:50:20 -0400

    ErrorHandler logs stack to terminal

--- jamming-app/apps/jamming-api/src/middleware/ErrorHandler.ts
+++ jamming-app/apps/jamming-api/src/middleware/ErrorHandler.ts
@@ -12,7 +12,7 @@ const ErrorHandler = (
     console.log(colors.red.underline('Middleware Error Handling'));
     const errStatus = err.httpCode;
     const errMsg = err.message;
-    console.error(util.inspect(err.stack, false, null, true));
+    console.error(colors.red(err.stack));
     res.status(errStatus).json({
       success: false,
       status: errStatus,
