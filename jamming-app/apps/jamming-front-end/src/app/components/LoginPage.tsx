35aaca0a8b3ed5753cad16c2827590ebc5f5cca5
mdwiltfong <wiltfong.michael1@gmail.com>
2023-08-12 17:18:58 -0400

    Additional Fields

--- jamming-app/apps/jamming-front-end/src/app/components/LoginPage.tsx
+++ jamming-app/apps/jamming-front-end/src/app/components/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useFormik } from 'formik';
@@ -14,26 +14,45 @@ export default function LoginPage() {
   });
   return (
     <>
-      <Container
-        maxWidth="sm"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          border: '1px solid black',
-          p: 1,
-        }}
-      >
-        <Box component={'form'} autoComplete="off">
+      <Container maxWidth="sm">
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            border: '1px solid black',
+            p: 1,
+          }}
+          onSubmit={formik.handleSubmit}
+        >
           <TextField
+            id="email"
+            name="email"
             label="Email"
+            type="email"
             value={formik.values.email}
-            defaultValue="Email"
-            className="outlined-required"
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
             required
           />
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            required
+          />
+          <Button type="submit" variant="contained">
+            {' '}
+            Submit{' '}
+          </Button>
         </Box>
-        <p>Login Page</p>
       </Container>
     </>
   );
